4c806b624bed2b858171ffe4c286e01f73aec345
Abhay274258 <abhayphirke2003@gmail.com>
2025-05-09 00:39:14 +0530

    Create Capstone.tsx

---
+++ src/pages/Capstone.tsx
@@ -0,0 +1,11 @@
+import CapstonePresentation from "@/components/CapstonePresentation";
+
+const Capstone = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <CapstonePresentation />
+    </div>
+  );
+};
+
+export default Capstone;
